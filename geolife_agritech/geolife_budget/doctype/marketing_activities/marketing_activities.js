8d3dca594cf8ea5f0a5af3bad34968f95222b7ae
Kamesh000 <kamesh@eactive.in>
2025-03-25 13:30:23 +0530

    geolife budget module addedd in this app

---
+++ geolife_agritech/geolife_budget/doctype/marketing_activities/marketing_activities.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Erevive Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Marketing Activities", {
+// 	refresh(frm) {
+
+// 	},
+// });
---
+++ geolife_agritech/geolife_budget/doctype/sales_target/test_sales_target.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Erevive Technologies and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestSalesTarget(FrappeTestCase):
+	pass
