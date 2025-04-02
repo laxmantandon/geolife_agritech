4ff20986c8e1fec0a95861a63afe9adcec2d771b
Kamesh000 <kamesh@eactive.in>
2025-04-02 14:05:48 +0530

    dealer_import error respolved

--- geolife_agritech/mrm/mrm_api.py
+++ geolife_agritech/mrm/mrm_api.py
@@ -21,40 +21,44 @@ def dealer_import(doc_name):
 	cust_avilable = []
 	cust_not_avilable = []
 	for res in result:
-		if frappe.db.count('Dealer', {'dealer_code': res.get("customer")}) > 0:
-			customer_name = frappe.db.get_value('Dealer', {'dealer_code': res.get("customer")}, ['name','dealer_name'], as_dict=1)
-			cust_avilable.append(customer_name)
-			if customer_name:
+		try:
+			if frappe.db.count('Dealer', {'dealer_code': res.get("customer")}) > 0:
+				customer_name = frappe.db.get_value('Dealer', {'dealer_code': res.get("customer")}, ['name','dealer_name'], as_dict=1)
+				cust_avilable.append(customer_name)
+				if customer_name:
+					monthly_sales = frappe.new_doc("Dealer Monthly Target")
+					monthly_sales.dealer = customer_name.name if customer_name.name else ''
+					monthly_sales.dealer_name = customer_name.dealer_name if customer_name.dealer_name else ''
+					monthly_sales.dealer_code = res.get("customer") if res.get("customer") else ''
+					monthly_sales.territory = res.get("territory") if res.get("territory") else ''
+					monthly_sales.fiscal_year = res.get("fiscal_year") if res.get("fiscal_year") else '' 
+					monthly_sales.month = res.get("month") if res.get("month") else ''
+					monthly_sales.outstanding = res.get("outstanding") if res.get("outstanding") else ''
+					monthly_sales.item_group = res.get("item_group") if res.get("item_group") else ''
+					monthly_sales.sale_quantity = res.get("s_quantity") if res.get("s_quantity") else ''
+					monthly_sales.sale_amount = res.get("s_amount") if res.get("s_amount") else ''
+					monthly_sales.return_quantity = res.get("r_quantity") if res.get("r_quantity") else ''
+					monthly_sales.return_amount = res.get("r_amount") if res.get("r_amount") else ''
+					monthly_sales.insert(ignore_permissions=True, ignore_links=True, ignore_if_duplicate=True, ignore_mandatory=True)
+				else:
+					cust_not_avilable.append(res.get("customer"))
+			else:
+				cust_not_avilable.append(res.get("customer"))
 				monthly_sales = frappe.new_doc("Dealer Monthly Target")
-				monthly_sales.dealer = customer_name.name if customer_name.name else ''
-				monthly_sales.dealer_name = customer_name.dealer_name if customer_name.dealer_name else ''
 				monthly_sales.dealer_code = res.get("customer") if res.get("customer") else ''
 				monthly_sales.territory = res.get("territory") if res.get("territory") else ''
 				monthly_sales.fiscal_year = res.get("fiscal_year") if res.get("fiscal_year") else '' 
 				monthly_sales.month = res.get("month") if res.get("month") else ''
-				monthly_sales.outstanding = res.get("outstanding") if res.get("outstanding") else ''
 				monthly_sales.item_group = res.get("item_group") if res.get("item_group") else ''
 				monthly_sales.sale_quantity = res.get("s_quantity") if res.get("s_quantity") else ''
 				monthly_sales.sale_amount = res.get("s_amount") if res.get("s_amount") else ''
 				monthly_sales.return_quantity = res.get("r_quantity") if res.get("r_quantity") else ''
 				monthly_sales.return_amount = res.get("r_amount") if res.get("r_amount") else ''
 				monthly_sales.insert(ignore_permissions=True, ignore_links=True, ignore_if_duplicate=True, ignore_mandatory=True)
-			else:
-				cust_not_avilable.append(res.get("customer"))
-		else:
-			cust_not_avilable.append(res.get("customer"))
-			monthly_sales = frappe.new_doc("Dealer Monthly Target")
-			monthly_sales.dealer_code = res.get("customer") if res.get("customer") else ''
-			monthly_sales.territory = res.get("territory") if res.get("territory") else ''
-			monthly_sales.fiscal_year = res.get("fiscal_year") if res.get("fiscal_year") else '' 
-			monthly_sales.month = res.get("month") if res.get("month") else ''
-			monthly_sales.item_group = res.get("item_group") if res.get("item_group") else ''
-			monthly_sales.sale_quantity = res.get("s_quantity") if res.get("s_quantity") else ''
-			monthly_sales.sale_amount = res.get("s_amount") if res.get("s_amount") else ''
-			monthly_sales.return_quantity = res.get("r_quantity") if res.get("r_quantity") else ''
-			monthly_sales.return_amount = res.get("r_amount") if res.get("r_amount") else ''
-			monthly_sales.insert(ignore_permissions=True, ignore_links=True, ignore_if_duplicate=True, ignore_mandatory=True)
-	
+		except Exception as e:
+			frappe.log_error('dealer_import mrm', f"{e}")
+			pass
+
 	# frappe.log_error('Customer_Avilable', cust_avilable)
 	# frappe.log_error('Customer_Not_Avilable', cust_not_avilable)
 
@@ -70,7 +74,7 @@ def custom_enqueue(doc_name):
 def mrm_api_dealer_target():
 	data = frappe.request.json
 	result = data.get("result")
-	# frappe.log_error("MY Result",result)
+	frappe.log_error("MY Result",result)
 	report_result = frappe.new_doc("Dealer Monthly Target Report Data")
 	report_result.api_report_data = json.dumps(result)
 	try:
