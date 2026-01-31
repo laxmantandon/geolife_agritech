aa311bb1566618a811eba8f713923d677ba23d59
Kamesh000 <kamesh@eactive.in>
2026-01-31 11:56:12 +0530

    all doctypes in statndard

---
+++ geolife_agritech/geolife_budget/doctype/territory_product_target/territory_product_target.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, Erevive Technologies and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Territory Product Target", {
+// 	refresh(frm) {
+
+// 	},
+// });
